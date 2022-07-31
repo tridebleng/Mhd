adf4fd515278d5e814145dd05e83d791b8e849b5
tridebleng <110109155+tridebleng@users.noreply.github.com>
2022-07-31 11:50:59 +0700

    Create copyrepo.sh

---
+++ salin/copyrepo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd
+sudo apt update -y
+apt update -y
+sudo apt install git -y
+apt install git -y
+cd
+mkdir /root/Mhd/
+cd /root/Mhd/
+git clone https://github.com/tridebleng/Mhd
+
+cd
